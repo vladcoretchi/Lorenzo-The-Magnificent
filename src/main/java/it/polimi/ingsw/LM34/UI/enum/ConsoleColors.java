0582590dcc39e97ff04ae5bcadd1c96f0e39ea03
RobertoDorata <roberto.dorata@gmail.com>
2017-05-18 07:27:11 +0200

    Describe the colors of all cards
    this enum will be used to print color of cards (blue, yellow, purple, green), and orange for 5th tower

---
+++ src/main/java/it/polimi/ingsw/LM34/UI/enum/ConsoleColors.java
@@ -0,0 +1,12 @@
+package enumers;
+
+//colori delle carte e dei messaggi di errore. Arancione per eventuale 5a torre
+
+public enum ConsoleColors {
+    BLUE,
+    GREEN,
+    YELLOW,
+    PURPLE,
+    ORANGE,
+    RED
+}
