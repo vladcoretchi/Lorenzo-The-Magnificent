0371d13487fa0fb1add9ad26c252d798e1f17d51
RobertoDorata <roberto.dorata@gmail.com>
2017-05-20 11:13:51 +0200

    cli improvements

---
+++ src/test/MyFirstTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by robertodorata on 5/20/17.
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyFirstTest {
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+}
