730962f25d12adbc5f9a3e591cbb7ed64d58c2de
Giulio Comi <giulio1.comi@mail.polimi.it>
2017-06-07 08:09:53 +0200

    works on gui

--- src/main/java/it/polimi/ingsw/LM34/UI/GUI/MainForm.java
+++ src/main/java/it/polimi/ingsw/LM34/UI/GUI/MainForm.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.LM34.UI.GUI;
 
+
 import it.polimi.ingsw.LM34.Enums.Controller.LeaderCardsAction;
 import it.polimi.ingsw.LM34.Enums.Controller.PlayerSelectableContexts;
 import it.polimi.ingsw.LM34.Enums.UI.NetworkType;
@@ -11,6 +12,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -29,10 +34,38 @@ public class MainForm extends Application implements UIInterface {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("gui/mainForm.fxml"));
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1500, 1400));
+        primaryStage.setFullScreen(true);
+        primaryStage.setResizable(false);
         primaryStage.show();
         //TODO
+
+        GridPane grid = (GridPane) root.lookup("#gridPaneResource");
+
+
+        // Just to see that the lines are actually added
+        grid.setGridLinesVisible(true);
+        grid.setPrefWidth(200);
+
+
+        for (Integer i = 1; i < 5; i++) {
+            ImageView imageView = new ImageView();
+            imageView.setImage(new Image(Thread.currentThread().getContextClassLoader().getResource("images/developmentCards/Commercial_Hub.png").toExternalForm()));
+            imageView.minHeight(200);
+            imageView.minWidth(250);
+            //grid.getRowConstraints().add(new RowConstraints(30));
+            grid.getRowConstraints().add(new RowConstraints(30));
+            grid.addRow(i, imageView);
+        }
+
+
+        primaryStage.show();
+
+
+
+        //primaryStage.show();
     }
 
     @Override
---
+++ src/main/resources/gui/GridPaneController.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by GiulioComi on 06/06/2017.
+ */
+public class GridPaneController  {
+    @FXML
+    private GridPane gridPane
+
+
+    @FXML
+    private void initialize() {
+
+    }
+}
